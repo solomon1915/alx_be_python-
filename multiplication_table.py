c7a99b240ee5a38fadfb2f0089cddd237bcecb0d
solomon1915 <solomonsisay191512@gmail.com>
2025-05-30 18:59:41 +0300

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -7,15 +7,3 @@ number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
     product = number * i
     print(f"{number} * {i} = {product}")
-Enter a number to see its multiplication table: 5
-5 * 1 = 5
-5 * 2 = 10
-5 * 3 = 15
-5 * 4 = 20
-5 * 5 = 25
-5 * 6 = 30
-5 * 7 = 35
-5 * 8 = 40
-5 * 9 = 45
-5 * 10 = 50
-
